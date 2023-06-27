7fa63c97b39004b08b1efdb36f770c7086cee18a
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-06-27 12:24:47 +0530

    [HEMANT]Add:Flip Coin and print percentage of Heads and Tails

---
+++ FunctionalProgramming/FlipCoin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Assignment
+{
+    public class FlipCoin
+    {
+        public void Checkflipcoin()
+        {
+            int toss;
+            int headscount = 0;
+            int tailscount = 0;
+            Console.WriteLine("Enter the number of times, you want to flip");
+            toss = Convert.ToInt32(Console.ReadLine());
+            if (toss > 0)
+            {
+                for (int i = 0; i < toss; i++)
+                {
+                    Random random = new Random();
+                    double check = random.Next(0, 2);
+                    if (check < 0.5)
+                    {
+                        headscount++;
+                    }
+                    else
+                    {
+                        tailscount++;
+                    }
+
+                }
+                int headsPercentage = (int)headscount / toss * 100;
+                int tailsPercentage = (int)tailscount / toss * 100;
+
+                Console.WriteLine("Number of flips: " + toss);
+                Console.WriteLine("Heads count: " + headscount);
+                Console.WriteLine("Tails count: " + tailscount);
+                Console.WriteLine("Percentage of Heads: " + headsPercentage + "%");
+                Console.WriteLine("Percentage of Tails: " + tailsPercentage + "%");
+            }
+            else
+            {
+                Console.WriteLine("Please enter the positive number");
+            }
+
+        }
+    }
+}
---
+++ FunctionalProgramming/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\n" + "1.Flip Coin and print percentage of Heads and Tails" + "\n");
+                Console.WriteLine("\n" + "2. FOR EXIT" + "\n");
+                Console.Write("Enter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        FlipCoin flipCoin = new FlipCoin();
+                        flipCoin.Checkflipcoin();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter correct options");
+                        break;
+                }
+            }
+        }
+    }
+
+}
